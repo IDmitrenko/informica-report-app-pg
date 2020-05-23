2e76963c63a6f1f625118ae46322180202a34a03
Dias <Dias64@mail.ru>
2020-05-23 23:07:20 +0300

    Config отчета для 10-го показателя

--- src/main/java/ru/avers/informica/scheduler/SchedulerTest.java
+++ src/main/java/ru/avers/informica/scheduler/SchedulerTest.java
@@ -3,8 +3,6 @@ package ru.avers.informica.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.avers.informica.launcher.ReportLauncher;
